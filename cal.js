285427c930877a7fcc931f06864fd887770630be
Jamezjaz <jamezjaz@gmail.com>
2020-10-12 10:26:30 -0500

    docs: defines the necessary variables and creates the cal class

---
+++ cal.js
@@ -0,0 +1,39 @@
+class Calculator {
+  constructor(previousOperandTextElement, currentOperandTextElement) {
+     this.previousOperandTextElement = previousOperandTextElement;
+     this.currentOperandTextElement = currentOperandTextElement;
+  }
+
+  clear() {
+    
+  }
+
+  delete() {
+
+  }
+
+  appendNumber() {
+
+  }
+
+  chooseOperation(operation) {
+
+  }
+
+  compute() {
+
+  }
+
+  updateDisplay() {
+
+  }
+}
+
+const numberButtons = document.querySelectorAll('[data-number]');
+const operationButtons = document.querySelector('[data-operation]');
+const equalsButton = document.querySelector('[data-equals]');
+const deleteButton = document.querySelector('[data-delete]');
+const allClearButton = document.querySelector('[data-all-clear]');
+const previousOperandTextElement = document.querySelector('[data-previous-operand]');
+const currentOperandTextElement = document.querySelector('[data-current-operand]');
+
