7c2ce73a4ac4bee2a8c71b40b2d9d111448a2b28
Jamezjaz <jamezjaz@gmail.com>
2020-10-12 10:34:05 -0500

    docs: creates new cal and passes the constructor into it

--- cal.js
+++ cal.js
@@ -39,5 +39,6 @@ const allClearButton = document.querySelector('[data-all-clear]');
 const previousOperandTextElement = document.querySelector('[data-previous-operand]');
 const currentOperandTextElement = document.querySelector('[data-current-operand]');
 
+const calculator = new Calculator(previousOperandTextElement, currentOperandTextElement);
 
 
