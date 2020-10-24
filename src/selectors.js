d52d7682ad1e27067f4585cc9c3b86da80ab9d18
Jamezjaz <jamezjaz@gmail.com>
2020-10-24 08:49:33 -0500

    refactor: footer module

---
+++ src/selectors.js
@@ -0,0 +1,7 @@
+export const numberButtons = document.querySelectorAll('[data-number]');
+export const operationButtons = document.querySelectorAll('[data-operation]');
+export const equalsButton = document.querySelector('[data-equals]');
+export const deleteButton = document.querySelector('[data-delete]');
+export const allClearButton = document.querySelector('[data-all-clear]');
+export const previousOperandTextElement = document.querySelector('[data-previous-operand]');
+export const currentOperandTextElement = document.querySelector('[data-current-operand]');
